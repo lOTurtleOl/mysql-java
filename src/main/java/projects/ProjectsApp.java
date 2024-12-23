f61449a7e8550695102abc8cb405d5f6931a295f
Andrew Hughes <andyashmore1@gmailcom>
2024-12-23 14:44:02 -0600

    add projects-schema create and drop tables

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -13,17 +13,9 @@ public class ProjectsApp {
 	}
 
 	public static void main(String[] args) {
+	
 		
-		// Had to add the below in order to avoid an error
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// Had to add the above in order to avoid an error
-		
-		Connection conn = DbConnection.getConnection();
+		DbConnection.getConnection();
 	}
 
 }
