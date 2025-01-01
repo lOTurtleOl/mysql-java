d1606b160f3a5f9616c6625743008879375d2c58
Andrew Hughes <andyashmore1@gmailcom>
2025-01-01 13:20:05 -0600

    add return project list and menu modifications

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -12,15 +12,17 @@ import projects.service.ProjectService;
 
 
 public class ProjectsApp {
-
+	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	// @formatter:off
-	private List<String> operations = List.of("1) Add a project"
+	private List<String> operations = List.of(
+			"1) Add a project", 
+			"2) List projects",
+			"3) Select a project"
 	);
 	// @formatter:on
-	
-	private Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 	
@@ -47,6 +49,14 @@ public class ProjectsApp {
 					createProject();
 					break;
 					
+				case 2:
+					listProjects();
+					break;
+					
+				case 3:
+					selectProject();
+					break;
+					
 				default:
 					System.out.println("\n" + selection + " is not a valid selection. Try again.");
 					break;
@@ -56,6 +66,31 @@ public class ProjectsApp {
 				System.out.println("\nError: " + e + " Try again.");
 			}
 		}		
+	}
+
+	/**
+	 * 
+	 */
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
+		/* Unselect the current project. */
+		curProject = null;
+		
+//		This will throw an exception if an invalid project ID is entered.
+		curProject = projectService.fetchProjectById(projectId);
+	}
+
+	/**
+	 * 
+	 */
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println("   " + project.getProjectId() + ": " + project.getProjectName()));
 	}
 
 	/**
@@ -155,6 +190,18 @@ public class ProjectsApp {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
 		
 		operations.forEach(line -> System.out.println(" " + line));
+		
+//		With enhanced for loop
+//		for(String line : operations) {
+//		System.out.println(" " + line);
+//		}
+		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with a project: " + curProject);
+		}
 	}
 
 }
